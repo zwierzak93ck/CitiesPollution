1fe5c81747b95ef789b651b7f275a623ebe4a351
KapitanKebab <zwierzak93ck@gmail.com>
2019-03-26 21:16:34 +0100

    Updates

--- precache-manifest.175e107c73f74ceaee737cfeebc13974.js
+++ precache-manifest.175e107c73f74ceaee737cfeebc13974.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/CitiesPollution/static/js/runtime~main.246c75f8.js"
   },
   {
-    "revision": "46cc783168e1417efb3f",
-    "url": "/CitiesPollution/static/js/main.69aca9bf.chunk.js"
+    "revision": "d6715152847ee28ee387",
+    "url": "/CitiesPollution/static/js/main.b2fe4ea9.chunk.js"
   },
   {
     "revision": "d75a5bed8bd03322700d",
     "url": "/CitiesPollution/static/js/2.5c5a465c.chunk.js"
   },
   {
-    "revision": "f9f0df3c1c38fb311937bc86dbd8d6ce",
+    "revision": "fd48ada7edee83471ac616a4b197bdd9",
     "url": "/CitiesPollution/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CitiesPollution/precache-manifest.5a62dbbc596e3ba403f4c0c86ffef479.js"
+  "/CitiesPollution/precache-manifest.175e107c73f74ceaee737cfeebc13974.js"
 );
 
 workbox.clientsClaim();
