0f3f41f2a96aa856ff05b6abb0374a391c388450
KapitanKebab <zwierzak93ck@gmail.com>
2019-03-26 21:08:28 +0100

    Updates

---
+++ precache-manifest.5a62dbbc596e3ba403f4c0c86ffef479.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "71287348f4e2a92df225",
+    "url": "/CitiesPollution/static/js/runtime~main.246c75f8.js"
+  },
+  {
+    "revision": "46cc783168e1417efb3f",
+    "url": "/CitiesPollution/static/js/main.69aca9bf.chunk.js"
+  },
+  {
+    "revision": "d75a5bed8bd03322700d",
+    "url": "/CitiesPollution/static/js/2.5c5a465c.chunk.js"
+  },
+  {
+    "revision": "f9f0df3c1c38fb311937bc86dbd8d6ce",
+    "url": "/CitiesPollution/index.html"
+  }
+];
